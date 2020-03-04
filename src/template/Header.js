25e8a418ed2fffae375aaf732cbda855b7c8ae3b
Samuel Mata <samuelab2@gmail.com>
2020-03-03 22:22:26 -0400

    fix the logo direction

--- src/template/Header.js
+++ src/template/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <div class="Header-main">
       <div class="Header-logo">
         <h1>
-          <a href="/">
+          <a href="https://samuelab2.github.io/RickAndMortyApp/">
             Rick and Morty App
           </a>
         </h1>
